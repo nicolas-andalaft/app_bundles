709c7141fe165f8e3d9dc07ea148fddd350b391f
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2021-05-28 14:13:02 -0300

    Redesigned App List Screen

--- lib/screens/app_list_screen.dart
+++ lib/screens/app_list_screen.dart
@@ -1,34 +1,54 @@
+import 'package:app_bundles/components/app_icon_image.dart';
+import 'package:app_bundles/models/app.dart';
+import 'package:device_apps/device_apps.dart';
 import 'package:flutter/material.dart';
-import 'package:installed_apps/app_info.dart';
-import 'package:installed_apps/installed_apps.dart';
 
 class AppListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('App List')),
-      body: FutureBuilder<List<AppInfo>>(
-        future: InstalledApps.getInstalledApps(true, true),
-        builder: (_, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (_, index) {
-                  return InkWell(
-                      onTap: () =>
-                          Navigator.pop(_, snapshot.data[index].packageName),
-                      child: ListTile(
-                        leading: Image.memory(snapshot.data[index].icon),
-                        title: Text(snapshot.data[index].name),
-                      ));
-                },
-              );
-            } else
-              return Text('Error loading installed apps');
-          } else
-            return LinearProgressIndicator();
-        },
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+          children: [
+            Text(
+              'Choose an app',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+            SizedBox(height: 10),
+            FutureBuilder<List<Application>>(
+              future: DeviceApps.getInstalledApplications(
+                includeAppIcons: true,
+                onlyAppsWithLaunchIntent: true,
+              ),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return Center(child: CircularProgressIndicator());
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: snapshot.data!.map(
+                    (e) {
+                      var app = App.fromApplication(e as ApplicationWithIcon);
+                      return ListTile(
+                        leading: AppIconImage(
+                          iconImage: app.iconImage,
+                          size: 40,
+                        ),
+                        title: Text(
+                          '${app.title}',
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        onTap: () => Navigator.of(context).pop(app),
+                      );
+                    },
+                  ).toList(),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
