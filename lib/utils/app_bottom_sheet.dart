78cd33d730022f9a334e9df55a4ccb31b4324b56
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2021-05-28 14:18:05 -0300

    Bottom Sheet for form buttons

---
+++ lib/utils/app_bottom_sheet.dart
@@ -0,0 +1,56 @@
+import 'package:app_bundles/models/app.dart';
+import 'package:device_apps/device_apps.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_launch_store/flutter_launch_store.dart';
+
+showAppBottomSheet(BuildContext context, App app) async {
+  bool isInstalled = await DeviceApps.isAppInstalled(app.appId!);
+
+  showModalBottomSheet(
+    context: context,
+    builder: (context) => Padding(
+      padding: const EdgeInsets.fromLTRB(30, 20, 30, 50),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '${app.title}',
+          ),
+          SizedBox(height: 20),
+          GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+            childAspectRatio: 3,
+            children: [
+              OutlinedButton.icon(
+                icon: Icon(Icons.launch),
+                label: Text('Open'),
+                onPressed:
+                    isInstalled ? () => DeviceApps.openApp(app.appId!) : null,
+              ),
+              OutlinedButton.icon(
+                icon: Icon(Icons.local_mall_outlined),
+                label: Text('Playstore'),
+                onPressed: () => StoreLauncher.openWithStore(app.appId!)
+                    .catchError((e) => print(e)),
+              ),
+              OutlinedButton.icon(
+                icon: Icon(Icons.star_border),
+                label: Text('Star'),
+                onPressed: () {},
+              ),
+              OutlinedButton.icon(
+                icon: Icon(Icons.edit_outlined),
+                label: Text('Edit'),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
