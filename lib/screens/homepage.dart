27b7b3da63d73ff52d0137b33fb0442e05ff5e2d
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2021-05-28 14:17:41 -0300

    Redesigned Homepage

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -3,8 +3,6 @@ import 'package:app_bundles/database/bundle_dao.dart';
 import 'package:app_bundles/models/bundle.dart';
 import 'package:app_bundles/models/route_names.dart';
 import 'package:flutter/material.dart';
-import 'package:receive_sharing_intent/receive_sharing_intent.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class Homepage extends StatefulWidget {
   @override
@@ -12,59 +10,55 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  void _checkSharedLink() async {
-    String link = await ReceiveSharingIntent.getInitialText();
+  List<Bundle> bundles = [];
 
-    // Check if string exists
-    if (link != null && link.isNotEmpty) {
-      if (link.contains('play.google.com') && link.contains('id=')) {
-        // Valid link
-        String id = link.split('id=')[1];
-        Navigator.of(context).pushNamed(RouteNames.appForm, arguments: id);
-      } else
-        // Invalid link
-        Fluttertoast.showToast(msg: 'Invalid App Link');
-    }
+  void _getBundles() {
+    BundleDao.readAll().then((value) => setState(() => bundles = value));
   }
 
   @override
   void initState() {
+    _getBundles();
     super.initState();
-    _checkSharedLink();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('App Bundles')),
-      body: FutureBuilder(
-        future: BundleDao.readAll(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.active)
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasData) {
-              List<Bundle> data = snapshot.data;
-              return ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (context, index) {
-                  return BundleCard(data[index]);
-                },
-              );
-            } else
-              return Center(child: Text('No Bundle Created'));
-          }
-          return Container();
-        },
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 50),
+          children: [
+            Text(
+              'AppBundles',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+            SizedBox(height: 40),
+            Text(
+              'Your Bundles',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            SizedBox(height: 10),
+            bundles.isEmpty
+                ? Text(
+                    "You don't have any bundles yet",
+                    style: Theme.of(context).textTheme.caption,
+                    textAlign: TextAlign.center,
+                  )
+                : Wrap(
+                    runSpacing: 20,
+                    children: bundles.map((e) => BundleCard(e)).toList(),
+                  ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => Navigator.of(context)
             .pushNamed(RouteNames.appForm)
-            .whenComplete(() => setState(() {})),
+            .then((value) => setState(() {
+                  _getBundles();
+                })),
       ),
     );
   }
